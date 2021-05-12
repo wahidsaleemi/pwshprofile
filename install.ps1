02e0309cedb9f1f34f8ddf04991fce9532209562
Wahid Saleemi <onewahid@live.com>
2021-05-12 07:21:55 -0500

    Rewrite install file to accomodate OneDrive redirection

--- install.ps1
+++ install.ps1
@@ -1,15 +1,44 @@
-#Point Console and ISE to the main profile.ps1
-$content = '$profile="$home\Documents\WindowsPowerShell\profile.ps1"'
 
-$pwshConsole = "$home\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
-$pwshISE = "$home\Documents\WindowsPowerShell\Microsoft.PowerShellISE_profile.ps1"
 
-New-Item -ItemType Directory -Path "$home\Documents\WindowsPowerShell\log"
+# The profile will log here
+New-Item -ItemType Directory -Path "$home\Documents\PowerShell\log" -ErrorAction SilentlyContinue | Out-Null
 
-Remove-Item -Path $pwshConsole
-Remove-Item -Path $pwshISE
-Write-Verbose "Updating Console and ISE profiles..."
-Add-Content -Value $content -Path $pwshConsole
-Add-Content -Value $content -Path $pwshISE
+   # Point Console and ISE to the main profile.ps1 for pwsh7
+$content = Get-Content -Path .\Microsoft.PowerShell_profile.ps1
 
-Copy-Item -Path ./profile.ps1 -Destination "$home\Documents\WindowsPowerShell\"
+# Find the path where profiles are stored
+$profilePaths = @(
+    "$home\Documents\WindowsPowerShell\profile.ps1",
+    "$home\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1",
+    "$home\Documents\WindowsPowerShell\Microsoft.PowerShellISE_profile.ps1"
+)
+# In many cases the profiles are offloaded to OneDrive
+if ($env:OneDrive) {
+    $defaultPaths = @(
+        "$env:OneDrive\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1",
+        "$env:OneDrive\Documents\WindowsPowerShell\Microsoft.PowerShellISE_profile.ps1"
+    )
+    Write-Host "Found profile in OneDrive paths."
+}
+else {
+    $defaultPaths = $profilePaths
+    Write-Host "Found profile in default paths. Updating..." 
+}
+
+foreach ($path in $defaultPaths) {
+        Write-Host "Removing old profile in $path"
+        Remove-Item -Path $path -ErrorAction SilentlyContinue
+
+       Write-Host "Updating default profile in $path"
+        New-Item -ItemType File -Path $path -Force | Out-Null
+        Add-Content -Value $content -Path $path
+}
+
+<#
+foreach ($profilepath in $profilePaths) {
+    Write-Host "Updating profile in $profilepath"
+    New-Item -ItemType File -Path $profilepath -Force | Out-Null
+    Add-Content -Value $content -Path $profilepath
+}
+#>
+Copy-Item -Path ./Microsoft.PowerShell_profile.ps1 -Destination "$home\Documents\PowerShell\"
